8fe7ddfd7dee13416f89c2803d5d4fe7ad66bf4f
Maurizio Mastria <mastria@2038.it>
2024-03-07 12:09:21 +0100

    Aggiunta Info Utente e clientId e clientSecret in env

---
+++ src/main/java/com/example/social/controller/SocialController.java
@@ -0,0 +1,17 @@
+package com.example.social.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SocialController {
+
+  @GetMapping("/social/user")
+  public String getUser(@AuthenticationPrincipal OAuth2User principal) {
+    return principal.getAttribute("url").toString();
+
+  }
+}
